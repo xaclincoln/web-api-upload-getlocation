2db439072a13fe0c1fc9076fb5dd397e7770a181
xac.lincoln <359972883@qq.com>
2017-03-14 16:09:09 +0800

    first commit

---
+++ GeoLocationService/Controllers/GeoLocationController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GeoLocationService.Controllers
+{
+
+    public class GeoLocationUploadRequest
+    {
+        /// <summary>
+        /// 设备序列号
+        /// </summary>
+        public string DeviceSN { get; set; }
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public double Longitude { get; set; }
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        public double Latitude { get; set; }
+    }
+
+    class GeoLocationRecord : GeoLocationUploadRequest
+    {
+        public DateTime Timestamp { get; set; }
+    }
+
+    [Route("api/[controller]/[action]")]
+    public class GeoLocationController : Controller
+    {
+        static ConcurrentQueue<GeoLocationRecord> _inMemoryRecords = new ConcurrentQueue<GeoLocationRecord>();
+
+        [HttpPost]
+        public IActionResult Upload(GeoLocationUploadRequest model)
+        {
+            _inMemoryRecords.Enqueue(new GeoLocationRecord
+            {
+                DeviceSN = model.DeviceSN,
+                Latitude = model.Latitude,
+                Longitude = model.Longitude,
+                Timestamp = DateTime.Now,
+            });
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult ReadAll()
+        {
+            return Json(_inMemoryRecords.AsEnumerable());
+        }
+    }
+}
---
+++ GeoLocationService/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GeoLocationService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://0.0.0.0:5000")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
